421d01c45e57a29e2f7373b7300dcf911e8812df
Marcos Cantarutti <marcossandim21@gmail.com>
2024-11-06 15:21:38 -0300

    criptografando

--- config/config.php
+++ config/config.php
@@ -5,5 +5,10 @@ return  [
         'driver' => 'sqlite',
         'database' => base_path('database/database.sqlite')
 
+    ],
+
+    'security' => [
+        'first_key' => 'O0xwEQ10/Q8pIlEy99abKPkbxFtfKE2DRJR8HdLRjIo=',
+        'second_key' => 'pQMutaugvx9bQlEIdU31S8b9GRM4h73n/hIstFawGgwhy9ewKHYzExAtkFSpQwpaZgRZerZZwsvymKH0rmRlKA==',
     ]
 ];
