4357591d14593c4b27f59eba4210cb2df490fd6b
Marcos Cantarutti <marcossandim21@gmail.com>
2024-11-07 14:20:06 -0300

    adicionando composer e autoload

--- public/index.php
+++ public/index.php
@@ -1,12 +1,13 @@
 <?php
-// criando autoload de classes
-require '../Core/functions.php';
 
-spl_autoload_register(function ($class) {
 
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
+require '../vendor/autoload.php';
+
+// spl_autoload_register(function ($class) {
+
+//     $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+//     require base_path("{$class}.php");
+// });
 
 
 session_start();
